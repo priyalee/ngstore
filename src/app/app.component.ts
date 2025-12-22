f819fa4658cbdafb775c2fdd3340140a0feb7ca0
Priyalee <priyalee.dahiya@scoreme.in>
2025-12-22 16:52:48 +0530

    update app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from "./navbar/navbar.component";
+// import { NavbarComponent } from "./navbar/navbar.component";
 // import { CartComponent } from './cart/cart.component';
 // import { UsersComponent } from "./user/user.component";
 // import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
