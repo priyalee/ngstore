0c630b198c89ec61fd488c302e80117bcc1d0522
Priyalee <priyalee.dahiya@scoreme.in>
2026-01-05 11:58:37 +0530

    route order changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,26 +1,28 @@
-// app.routes.ts
 import { Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { CartComponent } from './cart/cart.component';
-import { LoginComponent } from './login/login.component';
-import { UsersComponent } from './user/user.component';
+import { FrontBannerComponent } from './essentials/front-banner/front-banner.component';
 import { HomeBannerComponent } from './home-banner/home-banner.component';
+import { HomeComponent } from './home/home.component';
+import { ShopComponent } from './essentials/shop/shop.component';
+import { CartComponent } from './cart/cart.component';
+import { OrderComponent } from './essentials/order/order.component';
 import { AboutComponent } from './essentials/about/about.component';
 import { ContactComponent } from './essentials/contact/contact.component';
-import { ShopComponent } from './essentials/shop/shop.component';
-import { FrontBannerComponent } from './essentials/front-banner/front-banner.component';
-import { OrderComponent } from './essentials/order/order.component';
+import { LoginComponent } from './login/login.component';
+import { UsersComponent } from './user/user.component';
 
 export const routes: Routes = [
+
   { path: '', redirectTo: 'front-banner', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'cart', component: CartComponent },
-  { path: 'signin', component: LoginComponent },
-  { path: 'user', component:UsersComponent},
+  { path: 'front-banner', component: FrontBannerComponent },
   { path: 'home-banner', component: HomeBannerComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'shop', component: ShopComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'order', component: OrderComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
-  { path: 'shop', component: ShopComponent },
-  { path: 'front-banner', component: FrontBannerComponent },
-  { path: 'order', component: OrderComponent }
+  { path: 'signin', component: LoginComponent },
+  { path: 'user', component: UsersComponent },
+  { path: '**', redirectTo: 'front-banner' }
+
 ];
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../services/user.service';
-import { NavbarComponent } from "../navbar/navbar.component";
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [CommonModule, NavbarComponent],
   templateUrl: './user.component.html',
-  styleUrls: ['./user.component.scss']
+  styleUrls: ['./user.component.scss'],
 })
 export class UsersComponent implements OnInit {
   users: any[] = [];
@@ -22,21 +22,21 @@ export class UsersComponent implements OnInit {
 
   // Load all users
   loadUsers() {
-    this.userService.getAll().subscribe(data => {
+    this.userService.getAll().subscribe((data) => {
       this.users = data;
       console.log('All Users:', data);
     });
   }
 
-  //  Load single user by ID
+  // Load single user by ID
   loadUser(id: number) {
-    this.userService.getById(id).subscribe(user => {
+    this.userService.getById(id).subscribe((user) => {
       this.singleUser = user;
       console.log('Single User:', user);
     });
   }
 
-  //  Add user
+  // Add user
   addUser() {
     const newUser = {
       email: 'demo@example.com',
@@ -48,33 +48,33 @@ export class UsersComponent implements OnInit {
         street: 'Main Street',
         number: 42,
         zipcode: '12345',
-        geolocation: { lat: '20.000', long: '10.000' }
+        geolocation: { lat: '20.000', long: '10.000' },
       },
-      phone: '9876543210'
+      phone: '9876543210',
     };
 
-    this.userService.add(newUser).subscribe(res => {
+    this.userService.add(newUser).subscribe((res) => {
       console.log('User added:', res);
       this.loadUsers();
     });
   }
 
-  //  Update user
+  // Update user
   updateUser(id: number) {
     const updatedUser = {
       username: 'updatedUser',
-      email: 'updated@example.com'
+      email: 'updated@example.com',
     };
 
-    this.userService.update(id, updatedUser).subscribe(res => {
+    this.userService.update(id, updatedUser).subscribe((res) => {
       console.log('User updated:', res);
       this.loadUsers();
     });
   }
 
-  // dDelete user
+  // Delete user
   deleteUser(id: number) {
-    this.userService.delete(id).subscribe(res => {
+    this.userService.delete(id).subscribe((res) => {
       console.log('User deleted:', res);
       this.loadUsers();
     });
